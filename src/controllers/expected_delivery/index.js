6fdcf180c68a0fb4790e23bf4349d8f4195e8f10
farish-jamal <farishjamal98@gmail.com>
2025-11-22 13:24:47 +0530

    feat: Add expected delivery date calculation based on pincode and Rajkot city check

--- src/controllers/expected_delivery/index.js
+++ src/controllers/expected_delivery/index.js
@@ -1,3 +1,4 @@
+const finder = require('india-pincode-search');
 const ApiResponse = require('../../utils/apiResponse');
 const { asyncHandler } = require('../../utils/asyncHandler');
 const { getEstimatedPrice, trackDelivery } = require('../../utils/shipRocket');
@@ -11,10 +12,39 @@ exports.checkExpectedDelivery = asyncHandler(async (req, res) => {
          .status(200)
          .json(new ApiResponse(expectedDelivery.statusCode, null, expectedDelivery.message, false));
    }
-   const finalData = expectedDelivery.data.data.available_courier_companies[0].etd;
+
+   const pincode_with_state = finder.search(pincode);
+
+   const addDays = (date, days) => {
+      const result = new Date(date);
+      result.setDate(result.getDate() + days);
+      return result;
+   };
+
+   const formatDate = date =>
+      date.toLocaleDateString('en-US', {
+         month: 'short',
+         day: 'numeric',
+         year: 'numeric',
+      });
+
+   let estimatedDays = 0;
+   if (
+      pincode_with_state &&
+      (pincode_with_state[0].district === 'Rajkot' || pincode_with_state[0].city === 'Rajkot')
+   ) {
+      estimatedDays = 0;
+   } else {
+      const etd = expectedDelivery.data.data.available_courier_companies[0].estimated_delivery_days;
+      estimatedDays = Math.min(etd, 4);
+   }
+
+   const estimatedDate = addDays(new Date(), estimatedDays);
+   const formatted = formatDate(estimatedDate);
+
    return res
       .status(200)
-      .json(new ApiResponse(200, finalData, 'Expected delivery fetched successfully', true));
+      .json(new ApiResponse(200, formatted, 'Expected delivery fetched successfully', true));
 });
 
 exports.handleTrackDelivery = asyncHandler(async (req, res) => {
